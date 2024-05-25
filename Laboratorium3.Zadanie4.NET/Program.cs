2ad9826b1865888f42f2d96eb425105ecf66aee1
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-05-25 21:25:29 +0200

    Task 4 implementation

--- Laboratorium3.Zadanie4.NET/Program.cs
+++ Laboratorium3.Zadanie4.NET/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+namespace Laboratorium3.Zadanie4.NET
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+            var aes = Aes.Create();
+            const string filePath = "test.txt";
+        
+            var data = File.ReadAllBytes(filePath);
+            aes.GenerateKey();
+            var encryptedAesKey = rsa.Encrypt(aes.Key, true);
+            using var aesEncryptor = aes.CreateEncryptor();
+            var encryptedData = aesEncryptor.TransformFinalBlock(data, 0, data.Length);
+            File.WriteAllBytes(filePath + ".encrypted", encryptedData);
+            File.WriteAllBytes(filePath + ".encryptedAesKey", encryptedAesKey);
+
+            Console.WriteLine("Plik zaszyfrowany.");
+        
+            var encryptedDataFromFile = File.ReadAllBytes(filePath + ".encrypted");
+            var encryptedAesKeyFromFile = File.ReadAllBytes(filePath + ".encryptedAesKey");
+            var decryptedAesKey = rsa.Decrypt(encryptedAesKeyFromFile, true);
+            aes.Key = decryptedAesKey;
+            using var aesDecryptor = aes.CreateDecryptor();
+            var decryptedData = aesDecryptor.TransformFinalBlock(encryptedDataFromFile, 0, encryptedDataFromFile.Length);
+            File.WriteAllBytes(filePath + ".decrypted", decryptedData);
+
+            Console.WriteLine("Plik odszyfrowany.");
+        }
+    }
+}
